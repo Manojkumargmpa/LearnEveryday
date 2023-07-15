6f72da6500ee5b6a55e2769a0848f499f0659dba
manoj <manojkumargompa@gmail.com>
2023-07-15 22:06:42 +0530

    secondcommit

--- src/app.js
+++ src/app.js
@@ -1,44 +1,18 @@
 import { useEffect, useState } from 'react';
 import './styles.css';
-import * as mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-
-
-// const initialFacts = [
-//   {
-//     id: 1,
-//     text: 'React is being developed by Meta (formerly Facebook)',
-//     source: 'https://opensource.fb.com/',
-//     category: 'technology',
-//     votesInteresting: 24,
-//     votesMindblowing: 9,
-//     votesFalse: 4,
-//     createdIn: 2021,
-//   },
-//   {
-//     id: 2,
-//     text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
-//     source:
-//       'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-//     category: 'society',
-//     votesInteresting: 11,
-//     votesMindblowing: 2,
-//     votesFalse: 0,
-//     createdIn: 2019,
-//   },
-//   {
-//     id: 3,
-//     text: 'Lisbon is the capital of Portugal',
-//     source: 'https://en.wikipedia.org/wiki/Lisbon',
-//     category: 'society',
-//     votesInteresting: 8,
-//     votesMindblowing: 3,
-//     votesFalse: 1,
-//     createdIn: 2015,
-//   },
-// ];
-
-mongoose.connect('mongodb://127.0.0.1:27017/blogDB',{useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Further code execution or server startup
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 const factSchema = new mongoose.Schema({
   id: Number,
   text: String,
